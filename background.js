d6a4e0d8a597e20bd44376b83242091adc48a4f8
Solyphonous <82676469+Solyphonous@users.noreply.github.com>
2024-03-28 12:30:47 +0000

    Moved download function to other file, added browser_action

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+import { download } from "./download.js"
+
 console.log("CobaltExtension initialised successfully")
 
 browser.contextMenus.create({
@@ -10,47 +12,7 @@ browser.contextMenus.create({
 browser.contextMenus.onClicked.addListener(async (info, tab) => {
     switch (info.menuItemId) {
         case "download":
-            browser.tabs.query({ currentWindow: true, active: true }).then(tabs => {
-                let url = tabs[0].url
-                var uri = encodeURIComponent(url)
-
-                var data = {
-                    url: uri
-                }
-
-                fetch("https://co.wuk.sh/api/json", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Accept": "application/json",
-                    },
-                    body: JSON.stringify(data)
-                })
-                    .then(response => response.json())
-                    .then(json => {
-                        if (json.status != "error") {
-
-                            console.log("Trying to open new tab with url " + json.url)
-
-                            browser.tabs.create({
-                                url: json.url
-                            }).then(tab => {
-                                browser.tabs.onUpdated.addListener(function (tabId, changeInfo, updatedTab) {
-                                    if (tabId === tab.id && changeInfo.status === 'complete') {
-                                        browser.tabs.remove(tabId);
-                                        browser.tabs.onUpdated.removeListener(this);
-                                    }
-                                })
-                            })
-                        }
-                        else {
-                            //Logs error if returned from cobalt api
-                            console.error(json.text)
-                        }
-                    })
-                    //Logs error if POST request fails
-                    .catch(error => console.error("Error: ", error))
-            })
+            download()
             break
     }
 })
---
+++ download.js
@@ -0,0 +1,42 @@
+export function download() {
+    browser.tabs.query({ currentWindow: true, active: true }).then(tabs => {
+        let url = tabs[0].url
+        var uri = encodeURIComponent(url)
+
+        var data = {
+            url: uri
+        }
+
+        fetch("https://co.wuk.sh/api/json", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => response.json())
+            .then(json => {
+                if (json.status != "error") {
+
+                    console.log("Trying to open new tab with url " + json.url)
+
+                    browser.tabs.create({
+                        url: json.url
+                    }).then(tab => {
+                        browser.tabs.onUpdated.addListener(function (tabId, changeInfo, updatedTab) {
+                            if (tabId === tab.id && changeInfo.status === 'complete') {
+                                browser.tabs.remove(tabId);
+                            }
+                        })
+                    })
+                }
+                else {
+                    //Logs error if returned from cobalt api
+                    console.error(json.text)
+                }
+            })
+            //Logs error if POST request fails
+            .catch(error => console.error("Error: ", error))
+    })
+}
